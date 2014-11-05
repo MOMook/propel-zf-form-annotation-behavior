b31a0656cdedb683f3b9fddf7bf7986bbacfad9f
Simas Godovan <s.godovan@balticaa.com>
2014-11-05 19:12:39 +0200

    updated composer.json

--- src/ZfFormAnnotationBehavior.php
+++ src/ZfFormAnnotationBehavior.php
@@ -1,5 +1,13 @@
 <?php
 
+    namespace MOMook\Propel\Behavior;
+
+    use Propel\Generator\Model\Behavior;
+
+    /**
+     * Class ZfFormAnnotationBehavior
+     * @package MOMook\Propel\Behavior
+     */
     class ZfFormAnnotationBehavior extends Behavior
     {
 
